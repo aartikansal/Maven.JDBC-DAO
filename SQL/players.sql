618ae13a0085ee0533e0a2133a46619460d57c52
aartikansal <aartilumb@gmail.com>
2020-03-24 22:49:55 -0400

    changes made to Sql file

---
+++ SQL/players.sql
@@ -0,0 +1,11 @@
+create table players(id int auto_increment primary key,name text not null, country text not null, age int not null, points int not null, tourn_played int);
+INSERT INTO demo.players (id, name, country, age, points, tourn_played) VALUES (1, "Sachin Tendulkar", "Serbia", 31, 10990, 18);
+INSERT INTO demo.players (id, name, country, age, points, tourn_played) VALUES (2, "Rafael Nadal", "Spain", 32, 8725, 15);
+INSERT INTO demo.players (id, name, country, age, points, tourn_played) VALUES (3, "Alexander Zverev", "Germany", 21, 6630, 20);
+INSERT INTO demo.players (id, name, country, age, points, tourn_played) VALUES (4, "Dominic Thiem", "Austria", 25, 4755, 24);
+INSERT INTO demo.players (id, name, country, age, points, tourn_played) VALUES (5, "Roger Federer", "Switzerland", 37, 4600, 17);
+INSERT INTO demo.players (id, name, country, age, points, tourn_played) VALUES (6, "Kei Nishikori", "Japan", 29, 4235, 23);
+INSERT INTO demo.players (id, name, country, age, points, tourn_played) VALUES (7, "Kevin Anderson", "South Africa", 32, 4115, 18);
+INSERT INTO demo.players (id, name, country, age, points, tourn_played) VALUES (8, "Juan Martin del Potro", "Argentina", 30, 3585, 15);
+INSERT INTO demo.players (id, name, country, age, points, tourn_played) VALUES (9, "John Isner", "USA", 33, 3485, 23);
+INSERT INTO demo.players (id, name, country, age, points, tourn_played) VALUES (10, "Stefanos Tsitsipas", "Greece", 20, 3160, 29);
